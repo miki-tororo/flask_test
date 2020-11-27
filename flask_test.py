9834e65a8f3d5400932353336f7aa09bcf1b3aac
ict921 <ict921@ict.local>
2020-11-27 14:06:55 +0900

    wip read text

--- flask_test.py
+++ flask_test.py
@@ -1,16 +1,45 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+import codecs
+
 app=Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
     name_list=["a","b","c"]
-    
+
     return render_template("index.html",name_list=name_list)
 
-@app.route("/info")
-def info():
-    return "Flask"
+@app.route("/form")
+def form():
+    text="from"
+    file=codecs.open("articles.txt","r","utf-8")
+    lines=file.readlines()
+    file.close()
+
+    return render_template("form.html",text=text,lines=lines)
+
+
+
+@app.route("/menu2")
+def menu2():
+    text="text2"
+    return render_template("menu2.html",text=text)
+
+@app.route("/result", methods=["GET","POST"])
+def result():
+    text="result"
+
+    if request.method=="POST":
+        article=request.form["article"]
+        name=request.form["name"]
+    else:
+        article=request.args.get("article")
+        name=request.args.get("name")
+
+
+
+    return render_template("result.html",text=text,article=article,name=name)
 
 
 
